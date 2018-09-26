103fea0a1bb5d5ce4627056d58afecf9deb5b3d6
Linda <oneill.linda21@gmail.com>
2018-09-26 14:00:33 -0500

    add homepage styles

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
         <nav>
-           <Link to='/' style={{padding:'20px'}}>Home</Link>
-           <Link to='/library' style={{padding:'20px'}}>Library</Link>
+           <Link className="nav-link" to='/landing'>HOME</Link>
+           <Link className="nav-link" to='/library'>LIBRARY</Link>
          </nav>
-          <h1 style={{color:'indigo', fontFamily:'Roboto', fontWeight: 500}}>B l o c J a m s</h1>
+          <h1 className="title">P L A Y M U S E</h1>
         </header>
       <main>
         <Route exact path="/" component={Landing} />
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,22 +1,14 @@
 import React from 'react';
 
-
 const Landing = () => (
   <section className="landing">
-    <h2 className="hero-title" style={{color:'indigo', fontFamily:'Roboto', fontWeight: 300}}>Turn the music up!</h2>
+    <img src={"/assets/images/headphones.jpg"} className="background"/>
 
     <section className="selling-points">
       <div className="point">
-        <h3 className="point-title" style={{color:'indigo', fontFamily:'Roboto', fontWeight: 300}}>Choose your music</h3>
-        <p className="point-description" style={{color:'indigo', fontFamily:'Roboto', fontWeight: 300}}>The world is full of music; why should you have to listen to music that someone else chose?</p>
-      </div>
-      <div className="point">
-        <h3 className="point-title" style={{color:'indigo', fontFamily:'Roboto', fontWeight: 300}}>Unlimited, streaming, ad-free</h3>
-        <p className="point-description" style={{color:'indigo', fontFamily:'Roboto', fontWeight: 300}}>No arbitrary limits. No distractions.</p>
-      </div>
-      <div className="point">
-        <h3 className="point-title" style={{color:'indigo', fontFamily:'Roboto', fontWeight: 300}}>Mobile enabled</h3>
-        <p className="point-description" style={{color:'indigo', fontFamily:'Roboto', fontWeight: 300}}>Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+        <h3 className="point-title">UNLIMITED STREAMING</h3>
+        <h3 className="point-title">AD-FREE</h3>
+        <h3 className="point-title">MOBILE ENABLED</h3>
       </div>
     </section>
  </section>
